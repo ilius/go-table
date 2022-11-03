59fbef44930bec8303f0911e1e000663e7c0da00
Saeed Rasooli <saeed.gnu@gmail.com>
2022-11-04 00:55:52 +0330

    pass sep argument to FormatHeader

--- table.go
+++ table.go
@@ -142,10 +142,10 @@ func (t *Table) padColumnHeader(col *Column) string {
 	return AlignmentCenter(value, width)
 }
 
-func (t *Table) FormatHeader(color string) string {
+func (t *Table) FormatHeader(color string, sep string) string {
 	str := color
 	for _, col := range t.Columns {
-		str += t.padColumnHeader(col) + " "
+		str += t.padColumnHeader(col) + sep
 	}
 	str += reset + "\n"
 	return str
